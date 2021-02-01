0559d627bbe76c08ccf17db36ca54eb23a1620fe
Medhansh7 <medhansh.chaubey@riolabz.com>
2021-02-01 19:16:05 +0530

    second commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,49 +1,54 @@
 import { Link } from "react-router-dom";
 import Footer from './Footer'
 import './About.css'
-import Navbar from './Navbar'
+
 
 export default function About() {
     return (
         <div>
-            <Navbar />
-            <br />
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-8" style={{ paddingTop: "10%" }}>
-                        <h2>Allow us to introduce ourselves.</h2>
-                        <br />
-                        <h5>
-                            Prazer! {'{ '}Portuguese for: it’s nice to meet you{' }'}. We are D.M. a new sparkling wine brand from Brazil, with our grapes grown in the Valley of the Vineyard in Southern Brazil. An entirely new class of sparkling wine. Try us and see what we mean.
-                        </h5>
+            <div className="row flex-column-reverse flex-lg-row" style={{ marginLeft: "3px", marginRight: "3px" }}>
+                <div className="col-md-9 " >
+                    <div className="tes">Allow us to introduce ourselves.</div>
+                    <div className="tes">
+                        Prazer! {'{ '}Portuguese for: it’s nice to meet you{' }'}. We are D.M. a new sparkling wine brand from Brazil, with our grapes grown in the Valley of the Vineyard in Southern Brazil. An entirely new class of sparkling wine. Try us and see what we mean.
+                        </div>
+                    <br />
+                    <div className="row ">
+                        <div className="col-md-4 st">
+                            <button className="btn shop">SHOP</button>
+                        </div>
+                        <div className="col-md-8">
+                            <div className="row" >
+                                <marquee width="100%" direction="right" height="100px" loop="infinite">
+                                    <div className="track row">
+                                        <p className="line " >FREE SHIPING - FREE SHIPPING - FREE SHIPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING</p>
+                                    </div>
+                                </marquee>
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-md-4">
-                        <img src="https://dmbrut.com/images/final-home.jpg" className="img-fluid" alt="Responsive image" />
-                        <br /> <br />
-                        <h4>60%
+                    <br />
+                    <div className="row" >
+                        <div className="tes">Made in the traditional champagne method with a Brazilian touch, D.M. is sparkling wine that is uniquely flexible and versatile.
+                        You can drink D.M. however you like, whenever you like. Each sip brings the beauty, culture, and expression that is the best of old and new world winemaking.
+                         It’s what makes us unmistakably Brazilian.</div>
+                    </div>
+                </div>
+
+                <div className="col-md-3 text-center">
+                    <div className="st  ">
+                        <div className="row">
+                            <img src="https://dmbrut.com/images/final-home.jpg" className="img-fluid i" alt="Responsive image" />
+                            <br />
+                            <div style={{ marginTop: "1.3vw", textAlign: "right", fontSize: "3vh" }}>60%
                         Chardonnay<br />
                         40%
                         Pinot Noir,<br />
                         matured
-                        12 months</h4>
-                    </div>
-                </div>
-                <br /><br />
-                <div className="row">
-                    <div className="col-md-3">
-                        <button className="btn shop">SHOP</button>
-                    </div>
-                    <div className="col-md-9">
-                        <div className="row" >
-                            <p className="line" style={{ animationDirection: "normal" }}>FREE SHIPING - FREE SHIPPING - FREE SHIPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING - FREE SHIPPING</p>
+                        12 months</div>
                         </div>
+
                     </div>
-                </div>
-                <br /><br />
-                <div className="row" >
-                    <h5 className="t">Made in the traditional champagne method with a Brazilian touch, D.M. is sparkling wine that is uniquely flexible and versatile.
-                    You can drink D.M. however you like, whenever you like. Each sip brings the beauty, culture, and expression that is the best of old and new world winemaking.
-                         It’s what makes us unmistakably Brazilian.</h5>
                 </div>
             </div>
             <br />
@@ -51,3 +56,6 @@ export default function About() {
         </div>
     )
 }
+
+
+// combinedReducer
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,27 @@ export default function Footer() {
     return (
         <div className="footer footer-main">
             <div className="row e">
-                <div className="col-md-2 text-center"><input type="text" placeholder="Type in your email..." /></div>
-                <div className="col-md-1 te"><button className="btn btn-primary">Subscribe</button></div>
-                <div className="col-md-5"></div>
-                <div className="col-md-1 te">CC Dom Maria 2020</div>
-                <div className="col-md-1 te text-center">Facebook <br />Instagram</div>
-                <div className="col-md-1 te text-center">Privacy
+                <div className="col-md-2 text-center" >
+                    <div className="container">
+                        <input type="text" style={{ width: "100%", color: "white", border: "solid white", backgroundColor: "rgb(56,56,56)", marginBottom: "10px" }} placeholder="Type in your email..." />
+                    </div>
+
+                </div>
+                <div className="col-md-2 te">
+                    <div className="container">
+                        <button className="btn btn-primary">Subscribe</button></div>
+                </div>
+
+                <div className="col-md-8" >
+                    <div className="row" >
+                        <div className="col-md-6"></div>
+                        <div className="col-md-2 te text-center">CC Dom Maria 2020</div>
+                        <div className="col-md-2 te text-center">Facebook <br />Instagram</div>
+                        <div className="col-md-2 te text-center">Privacy
                 Shipping<br /> Policy<br />Disclaimer</div>
+
+                    </div>
+                </div>
 
             </div>
         </div>
